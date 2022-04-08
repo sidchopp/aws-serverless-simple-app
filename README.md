abb9c9fc0903650c08414d8a059d3577c249877c
Sid <sidchopp@gmail.com>
2022-04-08 15:17:55 -0600

    fornt end and back end connected using API gateway

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # About it
 
-Front-end: AWS Amplify to deploy static resources
-Back-end: AWS Lambda, API Gateway, MongoDB
+## Step 1.
+
+AWS Amplify to deploy static resources or a React Front-end
+
+## Step 2.
+
+AWS Lambda - A serverless function that can be invoked and scaled individually on-demand- is triggered based on a specific event
+
+## Step 3.
+
+Amazon API Gateway is used to create a RESTful API that will allow us to make calls to our Lambda function from our Front-end application. API Gateway will act as a middle layer between the React client/Front-end we created in Step 1 and the serverless back-end we created in Step 2. Now, we can trigger our lambda function with an API call.
+
+## Step 4(a).
+
+Created a DynamoDB table that enable our Lambda function to store data in it.It has consistent performance at any scale and there are no servers to manage when using it.
+
+## Step 4(b).
+
+Additionally, we will use the AWS Identity and Access Management (IAM) service to securely give our services the required permissions to interact with each other. Specifically, we are going to allow the Lambda function we created in Step 2 to write to our DynamoDB table, created in Step4, using an IAM policy(A document that defines what AWS resources an entity (e.g., service, user, group) has access to). In order to do this, AWS SDK was used (JavaScript) from our Lambda function.
+So, in Step 4(a) and 4(b, )we added two services: DynamoDB (for storage) and IAM (for managing permissions securely). Both are connected to our Lambda function, so that it can write to our database.
+
+## Step 5.
+
+In this step, we modify our static website, created in Step 1, to invoke ourAPI and display custom inputted text.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  var callAPI = (firstName, lastName) => {
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({ "firstName": firstName, "lastName": lastName });
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch(" https://w2i0q8t5wk.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
+      .then(response => response.text())
+      .then(result => alert(JSON.parse(result).body))
+      .catch(error => console.log('error', error));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    callAPI(firstName, lastName);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div >
+      <form >
+        <label htmlFor='firstName'>First Name :</label>
+        <input
+          type="text"
+          id="firstName"
+          name="fname"
+          placeholder="Your First name.."
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)} />
+        <label htmlFor='lastName'>Last Name :</label>
+        <input
+          type="text"
+          id="lastName"
+          name='lname'
+          placeholder="Your Last name.."
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}  >Call API</button>
+      </form>
     </div>
   );
 }
